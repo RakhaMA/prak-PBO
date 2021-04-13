c7ff44430609137f76c36e1aa2e21fdb889e90d9
RakhaMA <123190109@student.upnyk.ac.id>
2021-04-13 23:36:21 +0800

    tes

--- src/tugas1/Main.java
+++ src/tugas1/Main.java
@@ -5,16 +5,41 @@
  */
 package tugas1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author rakha mohammad
  */
 public class Main {
     public static void main(String[] args){
-        Kerucut kerucut = new Kerucut(10, 7);
-        kerucut.tampilkan();
         
-        Bola bola = new Bola((float) 3.5);
-        bola.tampilkan();
+        int tinggi = 0, jari = 0;
+        Scanner input = new Scanner(System.in);
+        
+        try{
+            System.out.printf("Masukkan nilai jari-jari Kerucut: ");
+            jari = input.nextInt();
+        }catch(InputMismatchException error){
+            System.out.println("Input yang anda masukkan bukan integer, errornya adalah " + error.getMessage());
+        }try{
+            System.out.printf("Masukkan nilai tinggi Kerucut : ");
+            tinggi = input.nextInt();
+        }catch(InputMismatchException error){
+            System.out.println("Input yang anda masukkan bukan integer, errornya adalah " + error.getMessage());
+        }finally{
+            Kerucut kerucut = new Kerucut(tinggi, jari);
+            kerucut.tampilkan();
+        }
+        
+        try{
+            System.out.printf("Masukkan nilai jari-jari Bola : ");
+            jari = input.nextInt();
+        }catch(InputMismatchException error){
+            System.out.println("Input yang anda masukkan bukan integer, errornya adalah " + error.getMessage());
+        }finally{
+            Bola bola = new Bola(jari);
+            bola.tampilkan();
+        }
     }
 }
